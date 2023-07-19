c9f7591b79f08256ebef1a9bdd44c28155161208
galvarado <gil-alvarado@outlook.com>
2023-07-19 16:48:20 -0400

    navigation - close when selected made, close when click outside element

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const items = document.querySelectorAll(".item");
 
-/* Toggle mobile menu */
+// Toggle mobile menu
 function toggleMenu() {
   if (menu.classList.contains("active")) {
     menu.classList.remove("active");
@@ -11,46 +11,29 @@ function toggleMenu() {
     menu.classList.add("active");
     toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
   }
+  event.stopPropagation();
 }
 
 // Add active class to the current button (highlight it)
 $(document).ready(function () {
-  $(".item").click(function (){
+  $(".item").click(function () {
     $(".item").removeClass("active"); // Remove "active" class from all items
     $(this).addClass("active"); // Add "active" class to the clicked item
+
+    // Close the menu once the user selects a listed item
+    toggleMenu();
   });
 });
 
-/* Activate Submenu */
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
-}
-
-/* Close Submenu From Anywhere */
-function closeSubmenu(e) {
-  if (menu.querySelector(".submenu-active")) {
-    let isClickInside = menu
-      .querySelector(".submenu-active")
-      .contains(e.target);
-
-    if (!isClickInside && menu.querySelector(".submenu-active")) {
-      menu.querySelector(".submenu-active").classList.remove("submenu-active");
+function closeMenu(e) {
+  if (menu.classList.contains("active")) {
+    if (!menu.contains(e.target) && !toggle.contains(e.target)) {
+      // Close the menu if clicked outside the menu and toggle button
+      toggleMenu();
     }
   }
 }
-/* Event Listeners */
+
+// Event Listeners
 toggle.addEventListener("click", toggleMenu, false);
-for (let item of items) {
-  if (item.querySelector(".submenu")) {
-    item.addEventListener("click", toggleItem, false);
-  }
-  item.addEventListener("keypress", toggleItem, false);
-}
-document.addEventListener("click", closeSubmenu, false);
+document.addEventListener("click", closeMenu, false);
